2b2855ef776afede10e8222c0d96ffa4c2c0d08e
Adityafr <adityapatwa.ap@gmail.com>
2019-07-29 10:29:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihangrid/latihanConstraintLayout.kt
@@ -0,0 +1,12 @@
+package com.example.latihangrid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class latihanConstraintLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latihan_constraint_layout)
+    }
+}
